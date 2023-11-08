929d1dfbbcebd9722cc9171df66f728042f61ff1
HuihaiW <hwang164@binghamton.edu>
2023-11-08 17:15:36 -0600

    opt result

--- src/RoutePlanning_clean.py
+++ src/RoutePlanning_clean.py
@@ -12,7 +12,6 @@ class RoadNetwork:
                  number_trucks, 
                  truck_capacity, 
                  str_time_limit):
-        
         self.numTrucks = number_trucks
         self.truck_capacity = truck_capacity
         self.strTimeLimit = str_time_limit
@@ -32,7 +31,9 @@ class RoadNetwork:
     def baseInit(self):
         self.baseObjLst = []
         for base in self.baseLst:
-            newBase = Base(base, self.numTrucks, 
+            newBase = Base(self.strTimeLimit, 
+                           base, 
+                           self.numTrucks, 
                            self.truck_capacity, 
                            self.CIDs, 
                            self.BCM, 
@@ -131,10 +132,12 @@ class RoadNetwork:
                     served_list.append(c)
 
 class Base():
-    def __init__(self, baseID, robotNum, robotCapaCty, CIDs, BCM, CCM, Demd):
+    def __init__(self, str_time_limit, baseID, robotNum, robotCapaCty, CIDs, BCM, CCM, Demd):
+        self.str_time_limit = str_time_limit
         self.ID = baseID
         self.robotCapaCty = robotCapaCty
         self.robotNum = robotNum
+
         self.BCM = BCM
         self.CCM = CCM
         self.Demd = Demd
@@ -168,6 +171,8 @@ class Base():
     def addTask(self, c, selected, networkDemand):
         if self.active:
             leftCap = self.robotCapaCty * self.robotNum - self.sumDemd
+            # if leftCap <= 0:
+            #     return selected, networkDemand
             demand = self.Demd[c]
             if leftCap > demand:
                 self.task.append(c)
@@ -184,170 +189,108 @@ class Base():
                 self.task.append(c)
                 self.taskDemd.append(leftCap)
                 networkDemand[c] = demand - leftCap
+                self.active = False
         return selected, networkDemand
 
     def getResult(self):
         if not self.active:
-            # get ccm
-            # get bcm
+            # get ccm from self.task
+            # get bcm from self.task
+            bcM = np.zeros((len(self.task)))
+            ccM = np.zeros((len(self.task), len(self.task)))
+            for i in self.task:
+                bcM[i] = self.BCM[self.task[i]]
+                for j in self.task:
+                    ccM[i][j] = self.CCM[self.task[i]][self.task[j]]
             # already get demand
+            task = [bcM, ccM, self.task, self.taskDemd]
             # append to task list
+            self.taskLst.append(task)
+            print("getting results")
             # result = SDVRP
+            resultCID, resultQuantity, resultTotDist = SDVRP(self.robotCapaCty,
+                                                             self.taskDemd,
+                                                             ccM,
+                                                             bcM,
+                                                             self.task,
+                                                             self.str_time_limit,
+                                                             self.robotNum)
             # append to result list
-            pass
-
-    def saveResult(self):
-        if not self.active:
-            # save result
-            pass
-
-    def optResult(self):
-        if not self.active:
-            # optimize results
+            self.resultLst.append([resultCID, resultQuantity, resultTotDist])
+            print("result is added correctly")
             self.baseInit()
 
-class base():
-    def __init__(self, ID, NumRobots, robot_capacity, bcM, networkIDList):
-        self.ID = ID
-        self.NumRobots = NumRobots
-        self.robot_capacity = robot_capacity
-        self.distance_sorted = sorted(bcM.tolist())
-        self.distance = bcM.tolist()
-        self.task_list = []
-        self.task_result_list = []
-        self.networkIDList = networkIDList
-        self.base_init()
+    def saveResult(self, saveFolderPath):
+        if not self.active:
+            if len(self.resultLst) == 0:
+                return 0
+            baseName = str(self.ID) + ".csv"
+            baseResultPath = os.path.join(saveFolderPath, baseName)
+            result = self.resultLst
+            round = []
+            customer = []
+            weight = []
+            length = []
+            for i in range(len(result)):
+                for j in range(len(result[i][0])):
+                    round.append(i)
+                    customer.append(result[i][0][j])
+                    weight.append(result[i][1][j])
+                    c = result[i][0][j]
+                    l = 0
+                    if len(c) > 1:
+                        l = self.BCM[self.ID][c[0]] + self.BCM[self.ID][c[-1]]
+                        for ic in range(0, len(c)-1):
+                            l += self.CCM[c[i]][c[i+1]]
+                    if len(c) == 1:
+                        l = self.BCM[self.ID][c[0]] + self.BCM[self.ID][c[-1]]
+                    length.append(l)
+            baseData = {'customer': customer, 'weight': weight,
+                        'round': round, 'length': length}
+            df = pd.DataFrame(baseData)
+            print("data result shape is: ", df.shape)
+            df.to_csv(baseResultPath)
 
-    def select(self, selected):
-        while True:
-            if len(self.distance_sorted) == 0:
-                return None
-            c = self.distance.index(self.distance_sorted[0])
-            # print(self.distance_sorted)
-            if c in selected:
-                self.distance_sorted.pop(0)
-                continue
-            else:
-                return c
-        
-    def add_customer(self, c, demand):
-        print("adding customers ...")
-        if (self.weights + demand[c]) > self.robot_capacity * self.NumRobots:
-            add_weight = self.robot_capacity * self.NumRobots - self.weights
-            networkDemand = demand
-            if add_weight > 0:
-                self.weights += add_weight
-                self.cID.append(c)
-                self.demands.append(add_weight)
-                networkDemand[c] = networkDemand[c] - add_weight
-            return False, networkDemand
-        # if len(self.distance_sorted) == 0:
-        #     self.weights += demand[c]
-        #     self.cID.append(c)
-        #     self.demands.append(demand[c])
-        #     return False
-        else:
-            self.distance_sorted.pop(0)
-            self.weights += demand[c]
-            self.cID.append(c)
-            self.demands.append(demand[c])
-            return True, demand
-
-    def add_task(self, ccM):
-        self.bcM = np.zeros((len(self.cID),))
-        self.ccM = np.zeros((len(self.cID), len(self.cID)))
-        for i in range(len(self.cID)):
-            self.bcM[i] = self.distance[self.cID[i]]
-        for i in range(len(self.cID)):
-            for j in range(len(self.cID)):
-                self.ccM[i][j] = ccM[i][j]
-        
-        task = [self.bcM, self.ccM, self.cID, self.demands]
-        self.task_list.append(task)
-
-    def add_task_result(self, demand, cc, bc, cID, str_time_limit, number_trucks):
-        print("adding task results ...")
-        customer_list, quantity_list, total_distance = SDVRP(self.robot_capacity, 
-                                                             demand,
-                                                             cc,
-                                                             bc,
-                                                             cID,
-                                                             str_time_limit,
-                                                             number_trucks)
-        self.task_result_list.append([customer_list, quantity_list, total_distance])
-        print("result adding correctly")
-
-    def base_init(self):
-        print("init base ... ")
-        self.bcM = np.empty((0, 0))
-        self.ccM = np.empty((0, 0))
-        self.cID = []
-        # self.cID_O = []
-        self.weights = 0
-        self.demands = []
-        print("base initialized")
-    def save_result(self, saveFolderPath, network):
-        baseName = str(self.ID) + '.csv'
-        baseResultPath = os.path.join(saveFolderPath, baseName)
-        result = self.task_result_list
-        round = []
-        customer = []
-        weight = []
-        length = []
-        for i in range(len(result)):
-            for j in range(len(result[i][0])):
-                round.append(i)
-                base_c = result[i][0][j]
-                customer.append(base_c)
-                weight.append(result[i][1][j])            
-                l = 0
-                if len(base_c) > 1:
-                    l = network.B_C_matrix[self.ID][base_c[0]] + network.B_C_matrix[self.ID][base_c[-1]]
-                    for c in range(len(base_c), len(base_c) - 1):
-                        l += network.C_C_matrix[base_c[c][c+1]]
-                elif len(base_c) > 0:
-                    l = network.B_C_matrix[self.ID][base_c[0]] + network.B_C_matrix[self.ID][base_c[-1]]
-                length.append(l)
-
-        base_data = {'customer': customer, 'weight' : weight, 'round': round, 'length': length}
-        df = pd.DataFrame(base_data)
-        print("data result shape is: ", df.shape)
-        print(baseResultPath)
-        df.to_csv(baseResultPath)
-    def optTasks(self, resultfolder, save_path):
+    def optResult(self, resultFolderPath):
         name = str(self.ID) + '.csv'
-        dpath = os.path.join(resultfolder, name)
-        df = pd.read_csv(dpath)
+        dPath = os.path.join(resultFolderPath, name)
+        if not os.path.exists(dPath):
+            return 0
+        df = pd.read_csv(dPath)
+        df["taskID"] = list(range(df.shape[0]))
         rounds = list(set(df['round'].values.tolist()))
-        lengthLL = []
-        # get length for each task in each round
-        for round in rounds:
-            l = df[df['round'] == round]['length'].values.tolist()
-            lengthLL.append(l)
-        # get number of robots for each base
-        num_robots = len(lengthLL[0])
-        
-        # deploy tasks for each robot, task are numbered as the order of origin tasks
-        optT = []
-            # init robot tasks
-        for i in range(num_robots):
-            optT.append([i])
-        robot_distance = [d for d in lengthLL[0]]
+        roundFirst = df[df["round"] == 0]
+        numRobot = roundFirst.shape[0]
 
-            # optimize length
+        # init robots based on first round of tasks
+        optTaskLst = []
+        optLenLst = []
+
+        roundTaskLst = roundFirst["taskID"].values.tolist()
+        roundLenLst = roundFirst["length"].values.tolist()
+        
+        optTaskLst = []
+        for a in roundTaskLst:
+            optTaskLst.append([a])
+        optLenLst.append(roundLenLst)
+
         if len(rounds) > 1:
-            for r_i in range(1, len(rounds)):
-                task_distance = lengthLL[r_i]
-                sort = task_distance.copy()
-                sort.sort(reverse=True)
-                distances = robot_distance.copy()
-                distances.sort()
-                for j in range(num_robots):
-                    robot_id = robot_distance.index(distances[j])
-                    task_id = task_distance.index(sort[j])
-                    robot_distance[robot_id] = robot_distance[robot_id] + task_distance[task_id]
-                    optT[robot_id].append(num_robots*r_i + task_id)
-        df_opt = pd.DataFrame({'task': optT, 'distance': robot_distance})
-        new_name = 'optTask_' + str(self.ID) + '.csv'
-        new_path = os.path.join(save_path, new_name)
-        df_opt.to_csv(new_path)
+            for round in rounds:
+                roundDF = df[df["round"] == round]
+                roundTaskLst = roundDF["taskID"].values
+                roundLenLst = roundDF["length"].values
+                
+                p = roundLenLst.argsort()
+
+                sortedRoundTaskLst = roundTaskLst[p]
+                sortedRoundLenLst = roundLenLst[p]
+                sortedOptLenLst = sorted(optLenLst, reverse=True)
+                for i in range(sortedRoundLenLst.shape[0]):
+                    rIndex = optLenLst.index(sortedOptLenLst[i])
+                    optTaskLst[rIndex].append(sortedRoundTaskLst[i])
+                    optLenLst[rIndex] += sortedRoundLenLst[i]
+        
+        optDF = pd.DataFrame({'task': optTaskLst, 'distance': optLenLst})
+        newName = 'optTask_' + str(self.ID) + '.csv'
+        newPath = os.path.join(resultFolderPath, newName)
+        optDF.to_csv(newPath)
